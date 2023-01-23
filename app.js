b4cebe675e41eae5055a90b78dc981be54f6eb77
Sophnel Merzier <45902931+bobocuillere@users.noreply.github.com>
2023-01-23 12:59:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ const port = 7000;
 const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
-  res.send('Hello World from IBM Cloud Essentials!');
+res.send('<html><body><h1>Hello World from IBM Cloud Essentials!</h1><form action="/submit" method="post"><input type="submit" value="Click Me"></form></body></html>');
+})
+app.post('/submit', (req, res) => {
+res.send('Button Clicked!');
 })
 
 app.listen(port, host);
-console.log(`Running on http://${host}:${port}`);
+console.log(Running on http://${host}:${port});
+
