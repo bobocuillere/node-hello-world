c07673eeb1ec1071cdb9528e8a3b1711f576bdd4
Sophnel Merzier <45902931+bobocuillere@users.noreply.github.com>
2023-01-24 02:36:56 +0100

    Create button-test.js

---
+++ button-test.js
@@ -0,0 +1,23 @@
+const { Builder, By, until } = require('selenium-webdriver');
+const assert = require('assert');
+const mocha = require('mocha');
+
+describe('Button Test', function() {
+    let driver;
+
+    before(async function() {
+        driver = await new Builder().forBrowser('chrome').build();
+        await driver.get('http://localhost:7000/');
+    });
+
+    it('should click the button and return "Button Clicked!"', async function() {
+        const button = await driver.findElement(By.css('input[type="submit"]'));
+        await button.click();
+        const message = await driver.findElement(By.css('body')).getText();
+        assert.equal(message, 'Button Clicked!');
+    });
+
+    after(async function() {
+        await driver.quit();
+    });
+});
