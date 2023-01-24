29c50c3f86a425ff7675e9d79bc748509736c50b
Sophnel Merzier <45902931+bobocuillere@users.noreply.github.com>
2023-01-24 04:15:02 +0100

    Update button.js

--- button.js
+++ button.js
@@ -2,26 +2,27 @@ const puppeteer = require('puppeteer');
 const assert = require('assert');
 
 describe('Button Test', async function() {
-let browser;
-let page;
+    let browser;
+    let page;
 
-before(async function() {
-browser = await puppeteer.launch({
-headless: true,
-args: ['--no-sandbox', '--disable-setuid-sandbox'],
-});
-page = await browser.newPage();
-await page.goto('http://0.0.0.0:7000/');
-});
+    before(async function() {
+        browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        });
+        page = await browser.newPage();
+        await page.goto('http://0.0.0.0:7000/');
+    });
 
-it('should click the button and return "Button Clicked!"', async function() {
-await page.click('input[type="submit"]');
-const message = await page.evaluate(() => document.querySelector('body').textContent);
-assert.equal(message, 'Button Clicked!');
-});
+    it('should click the button and return "Button Clicked!"', async function() {
+        await page.click('input[type="submit"]');
+        await new Promise(resolve => page.on('load', resolve));
+        const message = await page.evaluate(() => document.querySelector('body').textContent);
+        assert.equal(message, 'Button Clicked!');
+    });
 
-after(async function() {
-await browser.close();
-});
+    after(async function() {
+        await browser.close();
+    });
 });
 
