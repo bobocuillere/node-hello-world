6729c6216e7834759f8b237835b5f726368baff4
Sophnel Merzier <45902931+bobocuillere@users.noreply.github.com>
2023-01-26 06:14:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 7000;
 const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
-res.send('<html><body><h1>Bonjour, ma nouvelle feature avec un pipeline jenkins</h1><form action="/submit" method="post"><input type="submit" value="Click Me"></form></body></html>');
+res.send('<html><body><h1>Bonjour Jenkins</h1><form action="/submit" method="post"><input type="submit" value="Click Me"></form></body></html>');
 })
 app.post('/submit', (req, res) => {
 res.send('Button Clicked!');
