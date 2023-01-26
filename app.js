b6a1b9499ef47bab0c6b6d656723370662046e53
Sophnel Merzier <45902931+bobocuillere@users.noreply.github.com>
2023-01-26 05:46:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 'use strict';
 
-const express = require('express')
+const express = require('express');
 const app = express();
+
+// Serve static files from the "public" directory
+app.use(express.static("public"));
 
 const port = 7000;
 const host = '0.0.0.0';
-<link rel="stylesheet" type="text/css" href="/public/styles.css">;
 
 app.get('/', (req, res) => {
-res.send('<html><body><h1>Bonjour, ma nouvelle feature est tout beau, tout neuve !</h1><form action="/submit" method="post"><input type="submit" value="Click Me"></form></body></html>');
-})
+    res.send(`
+        <html>
+            <head>
+                <link rel="stylesheet" type="text/css" href="public/styles.css">
+            </head>
+            <body>
+                <h1>Bonjour, ma nouvelle feature est tout beau, tout neuve !</h1>
+                <form action="/submit" method="post">
+                    <input type="submit" value="Click Me">
+                </form>
+            </body>
+        </html>
+    `);
+});
 app.post('/submit', (req, res) => {
-res.send('Button Clicked!');
+    res.send('Button Clicked!');
 });
 app.listen(port, host);
-console.log('Running on http://${host}:${port}');
+console.log(`Running on http://${host}:${port}`);
