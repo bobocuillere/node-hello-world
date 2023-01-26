487b6c20ea4dc5db7c00f8ab17f508e64e1754ef
Sophnel Merzier <45902931+bobocuillere@users.noreply.github.com>
2023-01-26 04:35:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,18 @@ const app = express();
 const port = 7000;
 const host = '0.0.0.0';
 
+app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
-res.send('<html><body><h1>Bonjour, ma nouvelle feature est tout beau, tout neuve !</h1><form action="/submit" method="post"><input type="submit" value="Click Me"></form></body></html>');
+res.send('<html><body style="background-image:url(/background.jpg);"><h1>Bonjour, ma nouvelle feature est tout beau, tout neuve !</h1><form action="/submit" method="post"><input type="submit" value="Click Me"></form></body></html>');
 })
 app.post('/submit', (req, res) => {
 res.send('Button Clicked!');
 })
 
 app.listen(port, host);
-console.log('Running on http://${host}:${port}');
+console.log(Running on http://${host}:${port});
+
+
+
+
+
