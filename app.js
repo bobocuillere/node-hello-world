10c7b1b8d258933215974edbb8ff69370f07ca07
Sophnel Merzier <45902931+bobocuillere@users.noreply.github.com>
2023-01-26 05:39:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const port = 7000;
 const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
+<link rel="stylesheet" type="text/css" href="/public/styles.css">
 res.send('<html><body><h1>Bonjour, ma nouvelle feature est tout beau, tout neuve !</h1><form action="/submit" method="post"><input type="submit" value="Click Me"></form></body></html>');
 })
 app.post('/submit', (req, res) => {
